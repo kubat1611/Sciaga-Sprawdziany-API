b2a5ad298e990dfc42b8a49e43fbf02b29edeb5a
Mund3k <74401169+kubat1611@users.noreply.github.com>
2024-02-04 11:54:03 +0100

    ASDASD

--- server.js
+++ server.js
@@ -6,62 +6,59 @@ app.use(cors())
 
 app.get("/*", (req, res) => {
     res.json(
-      {
-"topics": [
-{
-"title": "Zmiany na mapie politycznej",
-"subtopics": [
-{
-"title": "Państwa",
-"notes": [
-{
-"id": 1,
-"content": "Obecnie na świecie jest około 160 państw"
-},
-{
-"id": 2,
-"content": "Aby można było uznać państwo potrzebne są 3 główne cechy: 1. Suwerenna władza 2. Wytyczone terytorium 3. Ludność, która jest świadoma swojej narodowości"
-},
-{
-"id": 3,
-"content": "Uznanie międzynarodowe jest ważnym aspektem w tworzeniu się nowego państwa"
-},
-{
-"id": 4,
-"content": "Lista największych kraji na świecie: 1.Rosja 2.Kanada 3.Stany Zjednoczone 4.Chiny 5.Brazylia"
-},
-{"id" : 5,
-"content" : "Najmniejszy kraj na swiecie to Watykan"
+        {
+            "topics": [
+                {
+                    "title": "Polska w XVII wieku",
+                    "subtopics": [
+                        {
+                            "title": "Historia Polski",
+                            "notes": [
+                                {
+                                    "id": 1,
+                                    "content": "XVII wiek to okres istotny dla historii Polski, zwłaszcza z uwagi na liczne wydarzenia polityczne, społeczne i kulturalne."
+                                },
+                                {
+                                    "id": 2,
+                                    "content": "Rządy króla Jana III Sobieskiego były jednym z najważniejszych momentów tego stulecia."
+                                },
+                                {
+                                    "id": 3,
+                                    "content": "W tym okresie Polska była państwem o stosunkowo dużym terytorium, sięgającym Bałtyku na północy i Morza Czarnego na południu."
+                                }
+                            ]
+                        },
+                        {
+                            "title": "Wojny i konflikty",
+                            "notes": [
+                                {
+                                    "id": 4,
+                                    "content": "Polska była zaangażowana w różne konflikty w XVII wieku, takie jak wojny z Turcją, Rosją i Szwecją."
+                                },
+                                {
+                                    "id": 5,
+                                    "content": "Bitwa pod Kircholmem w 1605 roku była jednym z ważniejszych starć z udziałem Polski w XVII wieku."
+                                }
+                            ]
+                        },
+                        {
+                            "title": "Kultura i nauka",
+                            "notes": [
+                                {
+                                    "id": 6,
+                                    "content": "W XVII wieku rozwijała się kultura i sztuka w Polsce, szczególnie w okresie baroku."
+                                },
+                                {
+                                    "id": 7,
+                                    "content": "W tym czasie działalność artystyczna takich postaci jak Jan III Sobieski, król-pisarz, miała duże znaczenie dla polskiej kultury."
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        });
 
-}
-]
-},
-{
-"title": "Kolonializm",
-"notes": [
-{
-"id": 5,
-"content": "Zjawisko kolonializmu polegało na przejmowaniu kontroli nad słabiej rozwiniętym regionem i wykorzystywaniu go gospodarczo"
-},
-{
-"id": 6,
-"content": " O początkach kolonializmu możemy mówić od odkrycia Ameryki przez Krzysztofa Kolumba"
-},
-{
-"id": 7,
-"content": "kolonizacja - Zakładanie osad, eksploatacja zasobów naturalnych, Podbój przez kraje europejskie oraz kontrola polityczna."
-},
-{
-"id" : 8,
-"content" : "dekolonizacja - Odzyskiwanie przez kraje niepodległości, W Afryce- głównie w drugiej połowie XX wieku"
-}
-]
-},
-]
-}
-]
-}
-  );
 });
 
 
@@ -70,4 +67,3 @@ app.listen(5000, () => {
     console.log("Server started on port 5000");
 });
 
-export default app
